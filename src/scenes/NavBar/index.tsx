914555d2a19cc662dd1cc4ca896476e8dbfdae67
Vlad <vladswork95@gmail.com>
2025-07-13 15:07:51 +0100

    style: header menu toggle

--- src/scenes/NavBar/index.tsx
+++ src/scenes/NavBar/index.tsx
@@ -3,6 +3,7 @@ import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 type Props = {
   selectedPage: SelectedPage; 
@@ -11,6 +12,8 @@ type Props = {
 
 const NavBar = ({selectedPage, setSelectedPage}: Props) => {
     const flexBetween = "flex items-center justify-between";
+    const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
   return (
     <nav>
         <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
@@ -18,7 +21,7 @@ const NavBar = ({selectedPage, setSelectedPage}: Props) => {
             <div className={`${flexBetween} w-full gap-16`}>
               <img src={Logo} alt="logo image" />
               {/*------------------RIGHT SIDE---------------*/}
-              <div className={`${flexBetween} w-full`}>
+              {isAboveMediumScreens ? (<div className={`${flexBetween} w-full`}>
                 <div className={`${flexBetween} gap-8 text-sm`}>
                   <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
                   <Link page="Benefits" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
@@ -29,7 +32,11 @@ const NavBar = ({selectedPage, setSelectedPage}: Props) => {
                   <p>Sign In</p>
                   <button type="button">Become a member</button>
                 </div>
-              </div>
+              </div>) : 
+              (<button 
+              className="rounded-full bg-secondary-500 p-2"
+              onClick={() => setIsMenuToggled(!isMenuToggled)}><Bars3Icon className="h-6 w-6 text-white"/></button>)}
+              
             </div>
           </div>
         </div>
