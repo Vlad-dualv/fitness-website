63b1082c0c0e3f0fad2a3215c4350557ca73bc8c
Vlad <vladswork95@gmail.com>
2025-07-14 13:25:39 +0100

    anim: Benefits

--- src/scenes/Benefits/index.tsx
+++ src/scenes/Benefits/index.tsx
@@ -76,18 +76,30 @@ export default function Benefits({setSelectedPage}: Props) {
                     {/*TITLE*/}
                     <div className="relative">
                         <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] before:content-abstractwaves">
-                            <div>
+                            <motion.div
+                            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{hidden: {opacity: 0, x: 50}, visible: {opacity: 1, x: 0}
+            }}>
                                 <HText>MILLIONS OF HAPPY MEMBERS GETTING{" "}
                                 <span className="text-primary-500">FIT</span>
                                 </HText>
-                            </div>
+                            </motion.div>
                         </div>
                     </div>
                     {/*DESCRIPTION*/}
-                    <div>
+                    <motion.div
+                    initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{delay: 0.2, duration: 0.5 }}
+            variants={{hidden: {opacity: 0, x: 50}, visible: {opacity: 1, x: 0}
+            }}>
                         <p className="my-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, quasi alias ea obcaecati praesentium, explicabo officiis quisquam saepe deleniti reiciendis iste aut, dolorum culpa voluptatem! Delectus facilis exercitationem quisquam odio?</p>
                         <p className="mb-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur porro ut itaque deleniti, doloribus culpa impedit voluptates explicabo excepturi voluptatum nulla ipsum dignissimos repellat exercitationem totam dolorum earum adipisci eius.</p>
-                    </div>
+                    </motion.div>
                     {/*BUTTON*/}
                     <div className="relative mt-16">
                         <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
