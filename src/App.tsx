951097dd953ad5294ecaeecbda7a42918a1cd4cb
Vlad <vladswork95@gmail.com>
2025-07-14 11:52:29 +0100

    style: benefits header layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import NavBar from "@/scenes/NavBar";
 import Home from "@/scenes/Home";
+import Benefits from "@/scenes/Benefits";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
+
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
@@ -23,6 +25,7 @@ function App() {
     <div className="app bg-gray-20">
       <NavBar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
       <Home setSelectedPage={setSelectedPage}/>
+      <Benefits setSelectedPage={setSelectedPage}/>
     </div>
   );
 }
