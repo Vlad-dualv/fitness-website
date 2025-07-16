4b03518ec82dc874900353e1d69b0c259f5862b4
Vlad <vladswork95@gmail.com>
2025-07-16 12:48:19 +0100

    style: OurClasses

--- src/scenes/OurClasses/index.tsx
+++ src/scenes/OurClasses/index.tsx
@@ -1,4 +1,4 @@
-import type { SelectedPage } from "@/shared/types";
+import type { SelectedPage, ClassType } from "@/shared/types";
 import image1 from "@/assets/image1.png";
 import image2 from "@/assets/image2.png";
 import image3 from "@/assets/image3.png";
@@ -7,6 +7,39 @@ import image5 from "@/assets/image5.png";
 import image6 from "@/assets/image6.png";
 import {motion} from "framer-motion";
 import HText from "@/shared/HText";
+import Class from "./Class";
+
+
+const classes: Array<ClassType> = [
+    {
+        name: "Weight Training Classes",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+        image: image1,
+    },
+    {
+        name: "Yoga Classes",
+        image: image2,
+    },
+    {
+        name: "Ab Core Classes",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+        image: image3,
+    },
+    {
+        name: "Adventure Classes",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+        image: image4,
+    },
+    {
+        name: "Fitness Classes",
+        image: image5,
+    },
+    {
+        name: "Training Classes",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+        image: image6,
+    },
+]
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
@@ -14,7 +47,7 @@ type Props = {
 
 function OurClasses({setSelectedPage}: Props) {
   return (
-    <section id="ourclasses" className="w-full bg-primary-100">
+    <section id="ourclasses" className="w-full bg-primary-100 py-20">
         <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}>
             <motion.div className="mx-auto w-5/6"
@@ -29,6 +62,17 @@ function OurClasses({setSelectedPage}: Props) {
                     <p className="py-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus quia exercitationem eos cupiditate obcaecati natus perspiciatis ex sequi aperiam ipsum soluta itaque fuga, ipsa ullam quasi optio quae qui illo.</p>
                 </div>
             </motion.div>
+            <div className="mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden">
+                <ul className="w-[2800px] whitespace-nowrap">
+                    {classes.map((item: ClassType, index) => (
+                        <Class 
+                        key={`${item.name}-${index}`}
+                        name={item.name}
+                        description={item.description}
+                        image={item.image}/>
+                    ))}
+                </ul>
+            </div>
         </motion.div>
     </section>
   )
